8ec6360e60482536cda3dff5748af0d60fb27f3c
Chris Watson <cwatson1988@gmail.com>
2024-03-08 14:13:49 +0000

    Refactor app.ts and update employees.test.ts

--- tests/employees.test.ts
+++ tests/employees.test.ts
@@ -1,14 +1,15 @@
 /*
-test the employee API  */
+Tests for the employee API  
+https://www.npmjs.com/package/supertest
+*/
 
-import { Request, Response } from 'express';
 import { expect, jest, test } from '@jest/globals';
 import { app } from '../src/app';
 import request from 'supertest';
 
 jest.mock('axios');
 
-test('Test the root path', () => {
+describe('Test the root path', () => {
   test('It should response the GET method', async () => {
     const response = await request(app).get('/');
     expect(response.statusCode).toBe(200);
