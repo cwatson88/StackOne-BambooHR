3df07da65b7e88422079ac573513366f1a181b37
Chris Watson <cwatson1988@gmail.com>
2024-03-08 22:17:05 +0000

    Refactor employee API endpoints and add data validation

--- src/api/employees/employees.service.ts
+++ src/api/employees/employees.service.ts
@@ -1,12 +1,17 @@
 import { z } from 'zod';
 import {
-  EmployeeDirectory,
   bamboohrApiRequest,
   convertToStackOneEmployee,
   convertToStackOneEmployments
 } from '../../utils/bamboohr-api/employees';
-import { AxiosError } from 'axios';
-import { Response, Request, response } from 'express';
+import { AxiosError, AxiosResponse } from 'axios';
+import { Response, Request } from 'express';
+import { EmployeeDirectory, BambooEmployee, BambooEmployeeEmployment } from '../../utils/interfaces/employees';
+import {
+  bambooEmployeeDirectorySchema,
+  bambooEmployeeEmploymentSchema,
+  bambooEmployeeSchema
+} from '../../utils/zod-schemas/employees.zod';
 
 const paramsSchema = z.object({
   id: z.coerce.number()
@@ -15,58 +20,72 @@ const paramsSchema = z.object({
 async function getEmployees(req: Request, res: Response): Promise<void> {
   try {
     // get the employee directory
-    const response = await bamboohrApiRequest('employees/directory');
+    const response: AxiosResponse<EmployeeDirectory> = await bamboohrApiRequest('employees/directory');
+
     if (!response.data) throw new Error('No employee data');
 
+    // validate the response data using zod schema
+    bambooEmployeeDirectorySchema.parse(response.data);
     console.log(response.data);
     // we only want to send the employees data
     res.send(response.data.employees);
-  } catch (error: AxiosError | any) {
-    console.error(error);
-    if (error.response) {
-      // AxiosError
-      const { response } = error;
-      res.status(response.status).send(response.data);
-    } else if (error.code === 'ECONNREFUSED') {
-      res.status(503).send("Service Unavailable. Can't connect to the API.");
-    } else {
-      res.status(500).send("Internal Server Error. Can't get employees.");
-    }
+  } catch (error) {
+    handleApiError(error, res);
   }
 }
 
-async function getEmployee(req: Request, res: Response) {
+async function getEmployee(req: Request, res: Response): Promise<void> {
   const { id } = req.params;
-
   try {
     // get the employee data by id
-    const bambooEmployeeData = await bamboohrApiRequest(
+    const response: AxiosResponse<BambooEmployee> = await bamboohrApiRequest(
       `employees/${id}?fields=displayName,firstName,lastName,preferredName,jobTitle,workPhone,mobilePhone,workEmail,department,location,division,linkedIn,instagram,pronouns,workPhoneExtension,supervisor,photoUploaded,photoUrl,canUploadPhoto,employmentHistoryStatus`
     );
 
-    if (!bambooEmployeeData.data) {
-      throw new Error('No employee data');
-    }
+    if (!response.data) throw new Error('No employee data');
 
-    const bambooEmployee = bambooEmployeeData.data;
-    const bambooEmploymentHistory = await bamboohrApiRequest(`employees/${bambooEmployee.id}/tables/employmentStatus/`);
+    // validate the response data using zod schema
+    bambooEmployeeSchema.parse(response.data);
 
-    const history = convertToStackOneEmployments(bambooEmploymentHistory?.data ?? []);
-    const employee = convertToStackOneEmployee(bambooEmployee, history);
+    const bambooEmployee = response.data;
 
+    const employmentHistory = await getEmploymentHistory(bambooEmployee);
+
+    const employee = convertToStackOneEmployee(bambooEmployee, employmentHistory);
     console.log(employee);
     res.send(employee);
-  } catch (error: AxiosError | any) {
-    console.error(error);
-    if (error.response) {
-      // AxiosError
-      const { response } = error;
-      res.status(response.status).send(response.data);
-    } else if (error.code === 'ECONNREFUSED') {
-      res.status(503).send("Service Unavailable. Can't connect to the API.");
-    } else {
-      res.status(500).send("Internal Server Error. Can't get employee data.");
-    }
+  } catch (error) {
+    handleApiError(error, res);
+  }
+}
+
+// get the employment history of the employee
+async function getEmploymentHistory(bambooEmployee: BambooEmployee) {
+  const bambooEmploymentHistory: AxiosResponse<BambooEmployeeEmployment[]> = await bamboohrApiRequest(
+    `employees/${bambooEmployee.id}/tables/employmentStatus/`
+  );
+
+  if (!bambooEmploymentHistory.data) throw new Error('No employee employment history data');
+
+  // validate the response data using zod schema
+  bambooEmployeeEmploymentSchema.parse(bambooEmploymentHistory.data);
+
+  const history = convertToStackOneEmployments(bambooEmploymentHistory?.data ?? []);
+
+  return history;
+}
+
+// handle errors from the BambooHR API
+function handleApiError(error: AxiosError | any, res: Response): void {
+  console.error(error);
+  if (error.response) {
+    // AxiosError
+    const { response } = error;
+    res.status(response.status).send(response.data);
+  } else if (error.code === 'ECONNREFUSED') {
+    res.status(503).send("Service Unavailable. Can't connect to the API.");
+  } else {
+    res.status(500).send("Internal Server Error. Can't get employee data.");
   }
 }
 
