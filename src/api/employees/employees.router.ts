867897549cd71829784e25069f614b570df0ab27
Chris Watson <cwatson1988@gmail.com>
2024-03-08 17:00:21 +0000

    Update employee route to use getEmployee function

--- src/api/employees/employees.router.ts
+++ src/api/employees/employees.router.ts
@@ -1,10 +1,10 @@
 // import express, service, and middleware
 import { Router } from 'express';
-import { getEmployees, paramsSchema } from './employees.service';
+import { getEmployees, paramsSchema, getEmployee } from './employees.service';
 import { validateParams } from '../../middleware/validations';
 
 const employeesRoute = Router()
   .get('/v1/employees', validateParams(paramsSchema), getEmployees)
-  .get('/v1/employees/:id', validateParams(paramsSchema), getEmployees);
+  .get('/v1/employee/:id', validateParams(paramsSchema), getEmployee);
 
 export default employeesRoute;
